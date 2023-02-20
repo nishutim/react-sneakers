16b2aab2ac7e38d6fc29a65df7431753f0ef6b1e
nishutim <nikmitush12345@gmail.com>
2023-02-20 12:35:25 +0300

    Initial commit. Basic clean up. Installing dependencies. Basic styles

---
+++ src/components/App/App.jsx
@@ -0,0 +1,11 @@
+import styles from './styles.module.scss';
+
+const App = () => {
+   return (
+      <div className={styles.wrapper}>
+         Working
+      </div>
+   );
+}
+
+export default App;
